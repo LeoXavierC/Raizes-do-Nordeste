77a809cab0ebd9e989fec6c0feb58a105897908c
Leo <leonardoxaviercap@gmail.com>
2026-06-16 19:20:39 -0300

    ajuste espaçamento

--- api/src/main/java/com/raizes_do_nordeste/api/domain/Estoque.java
+++ api/src/main/java/com/raizes_do_nordeste/api/domain/Estoque.java
@@ -3,7 +3,9 @@ package com.raizes_do_nordeste.api.domain;
 import jakarta.persistence.ManyToOne;
 
 public class Estoque {
+
     private Long id;
+
     private Integer quantidade;
 
     @ManyToOne
